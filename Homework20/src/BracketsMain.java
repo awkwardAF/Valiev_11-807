9474f1443b4dfa555a254018fbe805e37e6b1569
Azat Valiev <avaliev2013@litsey2.ru>
2019-02-11 21:21:52 +0300

    hw20

---
+++ Homework20/src/BracketsMain.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BracketsMain {
+    public static void main(String[] args) {
+        //пример: {)2+5(*[(2+3)*4]+1}/7
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        boolean correct = bracketsAreCorrect(s);
+    }
+
+    /**
+     * Возвращает true, если скобки в выражении расставлены корректно.
+     */
+    public static boolean bracketsAreCorrect(String s) {
+        boolean areCorrect = true;
+        Stack stack = new Stack(10);
+        char[] symbols = s.toCharArray();
+        for (int i = 0; i < symbols.length; i++) {
+            char c = symbols[i];
+            if (c == '(' || c == '{' || c =='['){
+                stack.push(c);
+            }
+            if (c == ')' && '(' != stack.pop()) {
+                return false;
+            }
+            if (c == ']' && '[' != stack.pop()) {
+                return false;
+            }
+            if (c == '}' && '{' != stack.pop()) {
+                return false;
+            }
+        }
+
+
+        return areCorrect;
+    }
+}
---
+++ Homework20/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(10);
+        System.out.println(BracketsMain.bracketsAreCorrect("{(2+5)*[(2+3)*4]+1}/7"));
+
+    }
+}
