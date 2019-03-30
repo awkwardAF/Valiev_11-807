24a66488bdd47f2cf80f0a473056d7d78006fe4f
awkwardAF <43217806+awkwardAF@users.noreply.github.com>
2019-03-30 13:31:34 +0300

    contains works

---
+++ Lesson32/src/BinarySearchTree.java
@@ -0,0 +1,9 @@
+
+
+public interface BinarySearchTree<T extends Comparable> {
+    void insert(T t);
+    void remove(T t);
+    boolean contains(T t);
+    void printAll();
+    void printAllByLevels();
+}
